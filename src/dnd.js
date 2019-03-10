fbdcae9e064fd5a67e0d17f64aa52b483021a0c7
Alexander Varnaev <alexandervarnaev@gmail.com>
2019-03-10 11:40:28 +0300

    dz5 complete

--- src/dnd.js
+++ src/dnd.js
@@ -27,6 +27,20 @@ const homeworkContainer = document.querySelector('#homework-container');
    homeworkContainer.appendChild(newDiv);
  */
 function createDiv() {
+    let newDiv = document.createElement('div');
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+
+    newDiv.className = 'draggable-div';
+    newDiv.style.top = Math.floor(Math.random() * 100) + 'px';
+    newDiv.style.left = Math.floor(Math.random() * 100) + 'px';
+    newDiv.style.width = Math.floor(Math.random() * 100) + 'px';
+    newDiv.style.height = Math.floor(Math.random() * 100) + 'px';
+    newDiv.style.backgroundColor = bgColor;
+
+    return newDiv;
 }
 
 /*
@@ -38,6 +52,33 @@ function createDiv() {
    addListeners(newDiv);
  */
 function addListeners(target) {
+    target.onmousedown = function(e) {
+      
+        target.style.position = 'absolute';
+        moveAt(e);
+        // переместим в body, чтобы мяч был точно не внутри position:relative
+        document.body.appendChild(target);
+
+        target.style.zIndex = 1000; // показывать мяч над другими элементами
+
+        // передвинуть мяч под координаты курсора
+        // и сдвинуть на половину ширины/высоты для центрирования
+        function moveAt(e) {
+            target.style.top = e.pageY - target.offsetHeight / 2 + 'px';
+            target.style.left = e.pageX - target.offsetWidth / 2 + 'px';
+        }
+
+        // 3, перемещать по экрану
+        document.onmousemove = function(e) {
+            moveAt(e);
+        };
+
+        // 4. отследить окончание переноса
+        target.onmouseup = function() {
+            target.onmouseup = null;
+            document.onmousemove = null;
+        };
+    };
 }
 
 let addDivButton = homeworkContainer.querySelector('#addDiv');
